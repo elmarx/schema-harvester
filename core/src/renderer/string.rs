571f00028e40cff42b54c1cdb0861699997f027c
Elmar Athmer <elmar@athmer.org>
2025-03-02 18:10:54 +0100

    WIP: (custom) serialize for string

--- core/src/renderer/string.rs
+++ core/src/renderer/string.rs
@@ -1,5 +1,7 @@
 use crate::model::StringNode;
 use crate::renderer::Render;
+use serde::Serialize;
+use serde::ser::SerializeStruct;
 use serde_json::json;
 
 impl Render for &StringNode {
@@ -16,6 +18,19 @@ impl Render for &StringNode {
                 })
             }
         }
+    }
+}
+
+impl Serialize for StringNode {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut state = serializer.serialize_struct("String", 2)?;
+        state.serialize_field("type", "string")?;
+        state.serialize_field("format", self.format.into())?;
+
+        todo!();
     }
 }
 
